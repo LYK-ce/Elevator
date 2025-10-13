932f887b4ddcf562d35040599a7164b7c72c7e20
LYK-ce <liyongkang_ce@163.com>
2025-10-13 10:26:33 +0800

    update utils

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ class Message:
     Message class for inter-process communication.
     '''
     def __init__(self, type: str, object: str, id: int, floor: int, state  : int = 0, delay = False):
-        self.type = type  # 'init', 'move', 'stop', 'call', 'arrive'
+        self.type = type  # 'init', 'passenger', 'elevator'
         self.object = object  # 'elevator' or 'passenger'
         self.id = id  # elevator id or passenger id
         self.floor = floor  # current floor
